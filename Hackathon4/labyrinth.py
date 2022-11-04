97f19432de0e444ad83cc0254adbbadd8a5bed6c
nosknut <nosknut@gmail.com>
2022-11-04 14:48:36 +0100

    Adds comment to the labyrinth example

--- Hackathon4/labyrinth.py
+++ Hackathon4/labyrinth.py
@@ -1,5 +1,6 @@
 from labyrinth_utils import turn_left, turn_right, move, detect
 
+# Example of a solution to the first and second labyrinth, which will not work for the third
 while True:
     if detect():
         turn_right()
