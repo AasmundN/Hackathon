aca6851a6434b57f0862c4eac05eb83adfe087ee
Knut Ola Nøsen <nosknut@gmail.com>
2022-11-09 11:32:36 +0100

    Updates labyrinths and adds right hand rule

--- Hackathon4/labyrinth.py
+++ Hackathon4/labyrinth.py
@@ -1,10 +1,20 @@
 from labyrinth_utils import turn_left, turn_right, move, detect, set_delay
 
-set_delay(0.5)
+set_delay(0.1)
 
 # Example of a solution to the first and second labyrinth, which will not work for the third
 while True:
-    if detect():
+    front = detect()
+    turn_right()
+    right = detect()
+    turn_left()
+
+    if not right:
         turn_right()
-    else:
         move()
+
+    if right and not front:
+        move()
+
+    if right and front:
+        turn_left()
